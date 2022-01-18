7c743f9814f18c4f490897f0c28d5cd08d0e6381
Andriy <shmagala2k1@gmail.com>
2022-01-18 20:12:23 +0200

    add popup IMG

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-let popupClose = document.querySelector('.popup__close');
+let popupClose = document.querySelectorAll('.popup__close');
 let popupOpen = document.querySelectorAll('.popup__open');
-let popup = document.querySelector('.popup');
+let popup = document.querySelector('.popup__form');
+let popupIMG = document.querySelector('.popup__img')
 let switchClass = '_active';
 let popupEclipse = document.querySelector('.popup__eclipse');
 let HTML = document.querySelector('html');
-let arr = [popupEclipse, popup, HTML];
+let arr = [popupEclipse,popup, HTML];
 let header = document.querySelector('header');
 let ourWorksItemArr = document.querySelectorAll('.our-works__title');
+let ItemsWorks = document.querySelectorAll('.our-works__item');
 
 let scrollToUp = 0;
 
-popupClose.addEventListener('click', () => {
-    arr.forEach(e => {
-        e.classList.remove(switchClass);
+popupClose.forEach(element => {
+    element.addEventListener('click', () => {
+        arr.forEach(e => {
+            e.classList.remove(switchClass);
+        })
+        popupIMG.classList.remove(switchClass)
     })
 })
 
@@ -23,6 +28,22 @@ popupOpen.forEach(e => {
         })
     })
 })
+
+ItemsWorks.forEach(e => {
+    e.addEventListener('click', (event)=>{
+        popupIMG.classList.add(switchClass)
+        HTML.classList.add(switchClass)
+        popupEclipse.classList.add(switchClass)
+        console.log(e, event);
+        openImgInPopup(event.path[1].children[0])
+    })
+})
+
+function openImgInPopup (img) {
+    //popupIMG.innerHTML = img
+    popupIMG.querySelector('img').src = img.src;
+    console.log(img)
+}
 
 window.addEventListener('scroll', headerSize);
 
